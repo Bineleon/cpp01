6477d16ccf5840b71d97677455eda7f646dc7eda
Nelbi Leon <nelbi.leon@gmail.com>
2025-03-28 15:32:54 +0100

    ex03 : DONE

---
+++ ex03/Weapon.cpp
@@ -0,0 +1,21 @@
+#include "Weapon.hpp"
+
+Weapon::Weapon(std::string const type) : _type(type)
+{
+    return;
+}
+
+Weapon::~Weapon()
+{
+
+}
+
+std::string Weapon::getType(void) const
+{
+    return _type;
+}
+
+void Weapon::setType(std::string type)
+{
+    _type = type;
+}
